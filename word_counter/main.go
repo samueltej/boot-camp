8122e5a6665bf0d712f1dc91a5458b7a3018c136
samueltej <pruebarcorreo.com>
2025-08-12 13:22:05 -0500

    Assigment word counter

---
+++ word_counter/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+)
+
+func main()  {
+
+	reader:= bufio.NewReader(os.Stdin)
+	fmt.Println("escribe la frase")
+	
+	var saltos string
+	if len(os.Args) > 1 {
+  	  saltos = os.Args[1]
+	} else {
+    	saltos = ""
+	}
+
+	contadorPalabras := 0
+	contadorSaltos := 0
+    salida := false
+
+    for !salida{
+	texto, _ := reader.ReadString('\n')
+	contadorSaltos++
+	words := strings.Fields(texto)
+ 
+		for _, word := range words{
+			if word != "exit"{
+			contadorPalabras++
+			} else {
+				salida=true
+				contadorSaltos= contadorSaltos-1
+			}
+		}
+	}
+
+	switch saltos {
+	case "-l":
+    	fmt.Println("numero de saltos", contadorSaltos)
+	case "":
+    	fmt.Println("numero de palabras", contadorPalabras)
+	default:
+    	fmt.Println("Opción no reconocida")
+	}
+	
+}
