cef17fbda1578baf75173b2a3c61da597c013b2c
samueltej <pruebarcorreo.com>
2025-09-29 13:28:20 -0500

    Behind the scenes of CombinedOutput()

--- exec/exec.go
+++ exec/exec.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os/exec"
-	"runtime"
+	"bytes"
+	"errors"
 )
 
-func main() {
-	cmd := exec.Command("ls", "-lah")
-	if runtime.GOOS == "windows" {
-		cmd = exec.Command("tasklist")
-	}
+func (c *Cmd) CombinedOutput() ([]byte, error) {
+	if c.Stdout != nil {
+		return nil, errors.New("exec: Stdout already set")
 
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
-	fmt.Printf("combined out:\n%s\n", string(out))
+	if c.Stderr != nil {
+		return nil, errors.New("exec: Stderr already set")
+
+
+	}
+	var b bytes.Buffer
+	c.Stdout = &b
+	c.stderr = &b
+	err := c.Run()
+	return b.Bytes(), err
 }
